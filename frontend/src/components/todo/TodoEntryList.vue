4e5fe62063ae9df03faee1742ba76958f233c33b
Martin Tománek <tomanma9@cvut.cz>
2025-03-30 19:47:16 +0200

    Continue work on TodoEntryLis

--- frontend/src/components/todo/TodoEntryList.vue
+++ frontend/src/components/todo/TodoEntryList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { TodoEntry } from "@/model/TodoEntry.ts"
+import { type TodoEntry, TodoEntryStatus } from "@/model/TodoEntry.ts"
 import { computed, type ComputedRef } from "vue"
 
 interface Props {
@@ -9,7 +9,7 @@ interface Props {
 
 const { entries, filter } = defineProps<Props>()
 
-const emit = defineEmits<{
+const emits = defineEmits<{
   delete: [id: number]
   update: [updatedEntry: TodoEntry]
   changeStatus: [entry: TodoEntry]
@@ -29,8 +29,29 @@ const filteredEntries: ComputedRef<TodoEntry[]> = computed(() => {
         <tr class="w3-topbar">
           <th>Status</th>
           <th>Item</th>
+          <th>Actions</th>
         </tr>
       </thead>
+      <tbody>
+        <tr v-for="entry in filteredEntries" :key="entry.id">
+          <td class="button" @click="$emit('changeStatus', entry)">
+            <i
+              class="fa-2x fa-square w3-grey"
+              v-if="entry.status == TodoEntryStatus.TODO"
+            ></i>
+            <i
+              class="fa-2x fa-minus w3-teal"
+              v-if="entry.status == TodoEntryStatus.DOING"
+            ></i>
+            <i
+              class="fa-2x fa-check w3-green"
+              v-if="entry.status == TodoEntryStatus.DONE"
+            ></i>
+          </td>
+          <td>{{ entry.name }}</td>
+          <td></td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
