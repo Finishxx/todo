5588539d203af6d5202806f89a37b0e5aaa8ae31
Martin Tománek <tomanma9@cvut.cz>
2025-03-30 17:28:10 +0200

    Update TodoApp

--- frontend/src/components/TodoApp.vue
+++ frontend/src/components/TodoApp.vue
@@ -3,11 +3,13 @@ import { computed, type ComputedRef, type Ref, ref } from "vue"
 import { type TodoEntry, TodoEntryStatus } from "@/model/TodoEntry.ts"
 import TodoSummary from "@/components/todo/TodoSummary.vue"
 import TodoFilter from "@/components/todo/TodoFilter.vue"
+import TodoEntryList from "@/components/todo/TodoEntryList.vue"
 
 const entries: Ref<TodoEntry[]> = ref([
   { id: 1, name: "Hello", status: TodoEntryStatus.TODO },
   { id: 2, name: "World", status: TodoEntryStatus.DONE },
 ])
+const filterText: Ref<string> = ref("")
 const entriesTodo = computed(() => {
   return entries.value.filter(item => item.status == TodoEntryStatus.TODO)
 })
@@ -15,77 +17,30 @@ const entriesDone = computed(() => {
   return entries.value.filter(item => item.status == TodoEntryStatus.DONE)
 })
 
-const newTodoText: Ref<string> = ref("")
+function addEntry() {}
 
-const filterText: Ref<string> = ref("")
-const filteredEntries: ComputedRef<TodoEntry[]> = computed(() => {
-  if (filterText.value == "") return entries.value
+function deleteEntry(entryId: number) {}
 
-  return entries.value.filter(entry => entry.name.includes(filterText.value))
-})
+function updateEntry(entry: TodoEntry) {}
 
-function clearTodo(): void {
-  newTodoText.value = ""
-}
-
-function addTodo(): void {
-  if (newTodoText.value !== "") {
-    entries.value.push({
-      id: new Date().valueOf(),
-      name: newTodoText.value,
-      status: TodoEntryStatus.TODO,
-    })
-    clearTodo()
-  }
-}
+function changeStatus(entry: TodoEntry) {}
 </script>
 
 <template>
   <div class="todo-container w3-white w3-card-4">
     <TodoSummary :entries="entries" />
+
+    <button @click="addEntry" class="w3-button add-button">Add new TODO</button>
+
     <TodoFilter v-model="filterText" />
 
-    <!-- Input -->
-    <div class="w3-container 3-light-gray w3-padding">
-      <input
-        class="w3-input w3-border-0"
-        type="text"
-        autofocus
-        v-model="newTodoText"
-        @keyup.enter="addTodo()"
-        placeholder="Add TODO item..."
-      />
-      <button class="w3-button w3-gray" @click="clearTodo()">
-        <i class="fa-solid fa-times"></i>
-      </button>
-      <button class="w3-button w3-blue" @click="addTodo()">
-        <i class="fa-solid fa-plus"></i>
-      </button>
-    </div>
-
-    <!-- List of TODO -->
-    <div class="w3-padding w3-blue">TODO ({{ entriesTodo.length }}</div>
-    <div class="w3-padding" v-for="todo in entriesTodo" :key="todo.id">
-      <label>
-        <input type="checkbox" v-model="todo.status" />
-        <span class="w3-margin-left">
-          {{ todo.name }}
-        </span>
-      </label>
-    </div>
-    <div class="w3-padding" v-show="entriesTodo.length == 0">No tasks</div>
-
-    <!-- List of DONE -->
-    <div class="w3-padding w3-blue">Done ({{ entriesDone.length }}</div>
-    <div class="w3-padding" v-for="todo in entriesDone" :key="todo.id">
-      <label>
-        <input type="checkbox" v-model="todo.status" />
-        <span class="w3-margin-left">
-          {{ todo.name }}
-        </span>
-      </label>
-    </div>
-    <div class="w3-padding" v-show="entriesDone.length == 0">No tasks</div>
+    <TodoEntryList
+      :entries="entries"
+      :filter="filterText"
+      @delete="deleteEntry"
+      @update="updateEntry"
+      @changeStatus="changeStatus"
+    ></TodoEntryList>
   </div>
 </template>
 
@@ -95,8 +50,8 @@ function addTodo(): void {
   min-width: 30rem;
 }
 
-label {
-  cursor: pointer;
-  display: flex;
+.add-button {
+  display: block;
+  width: 100%;
 }
 </style>
--- frontend/src/components/todo/TodoEntryList.vue
+++ frontend/src/components/todo/TodoEntryList.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
 import type { TodoEntry } from "@/model/TodoEntry.ts"
+import { computed, type ComputedRef } from "vue"
 
-const props = defineProps<{
+interface Props {
   entries: TodoEntry[]
-}>()
+  filter: string
+}
+
+const { entries, filter } = defineProps<Props>()
 
 const emit = defineEmits<{
   delete: [id: number]
   update: [updatedEntry: TodoEntry]
+  changeStatus: [entry: TodoEntry]
 }>()
+
+const filteredEntries: ComputedRef<TodoEntry[]> = computed(() => {
+  if (filter == "") return entries
+
+  return entries.filter(entry => entry.name.includes(filter))
+})
 </script>
 
-<template></template>
+<template>
+  <div>
+    <table class="w3-table">
+      <thead>
+        <tr class="w3-topbar">
+          <th>Status</th>
+          <th>Item</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
 
 <style scoped></style>
