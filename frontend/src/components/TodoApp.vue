6395a522f659cf998f3943a5a8dc1ac5b4ffccc7
Martin Tománek <tomanma9@cvut.cz>
2025-03-31 20:51:28 +0200

    Fix network error showing on every modal cancel action

--- frontend/src/components/TodoApp.vue
+++ frontend/src/components/TodoApp.vue
@@ -5,13 +5,13 @@ import TodoSummary from "@/components/todo/TodoSummary.vue"
 import TodoFilter from "@/components/todo/TodoFilter.vue"
 import TodoEntryList from "@/components/todo/TodoEntryList.vue"
 import { openModal } from "@kolirt/vue-modal"
-import TodoConfirmDeleteModal from "@/components/todo/modal/TodoConfirmDeleteModal.vue"
 import TodoEditModal, {
   type EditTodoEntryData,
 } from "@/components/todo/modal/TodoEditModal.vue"
 import { TodoService } from "@/service/TodoService.ts"
 import { TYPE, useToast } from "vue-toastification"
 import { type ActiveLoader, useLoading } from "vue-loading-overlay"
+import TodoConfirmDeleteModal from "@/components/todo/modal/TodoConfirmDeleteModal.vue"
 
 const entries: Ref<Map<number, TodoEntry>> = ref(new Map([]))
 
@@ -21,13 +21,13 @@ const entryList: ComputedRef<TodoEntry[]> = computed(() => {
 
 onMounted(async () => {
   try {
-    loader = loading.show()
+    startLoading()
     const allEntries: TodoEntry[] = await TodoService.loadTodos()
     entries.value = new Map(allEntries.map(entry => [entry.id, entry]))
   } catch {
     showNetworkErrorToast()
   } finally {
-    loader.hide()
+    stopLoading()
   }
 })
 
@@ -38,88 +38,96 @@ const loading = useLoading({
 })
 let loader: ActiveLoader
 
+function startLoading() {
+  loader = loading.show()
+}
+
+function stopLoading() {
+  loader.hide()
+}
+
 function showNetworkErrorToast() {
   toast("Network error!", { type: TYPE.ERROR })
 }
 
-function addEntry() {
-  openModal<EditTodoEntryData, typeof TodoEditModal>(TodoEditModal, {
-    name: "",
-    status: TodoEntryStatus.TODO,
-    title: "Add TODO",
-  })
-    .then(data => {
-      loader = loading.show()
-      return TodoService.newTodo(data.name, data.status)
-    })
-    .then(newTodo => {
-      entries.value.set(newTodo.id, newTodo)
-      toast("Add successful!", { type: TYPE.INFO })
-    })
-    .catch(error => {
-      console.log(error)
-      showNetworkErrorToast()
-    })
-    .finally(() => {
-      loader.hide()
-    })
+async function addEntry() {
+  let modalData
+  try {
+    modalData = await openModal<EditTodoEntryData, typeof TodoEditModal>(
+      TodoEditModal,
+      {
+        name: "",
+        status: TodoEntryStatus.TODO,
+        title: "Add TODO",
+      },
+    )
+  } catch {
+    return
+  }
+  startLoading()
+  try {
+    const newTodo = await TodoService.newTodo(modalData.name, modalData.status)
+    entries.value.set(newTodo.id, newTodo)
+    toast("Add successful!", { type: TYPE.INFO })
+  } catch {
+    showNetworkErrorToast()
+  } finally {
+    stopLoading()
+  }
 }
 
-function deleteEntry(entry: TodoEntry) {
-  openModal(TodoConfirmDeleteModal, {
-    areYouSureMessage: `Are you sure you want to delete entry "${entry.name}"`,
-  })
-    .then(() => {
-      loader = loading.show()
-      return TodoService.deleteTodo(entry.id)
+async function deleteEntry(entry: TodoEntry) {
+  try {
+    await openModal(TodoConfirmDeleteModal, {
+      areYouSureMessage: `Are you sure you want to delete entry "${entry.name}"`,
     })
-    .then(() => {
-      entries.value.delete(entry.id)
-      toast("Delete successful!", { type: TYPE.INFO })
-    })
-    .catch(error => {
-      console.log(error)
-      showNetworkErrorToast()
-    })
-    .finally(() => {
-      loader.hide()
-    })
+  } catch {
+    return
+  }
+  startLoading()
+  try {
+    await TodoService.deleteTodo(entry.id)
+    entries.value.delete(entry.id)
+    toast("Delete successful!", { type: TYPE.INFO })
+  } catch {
+    showNetworkErrorToast()
+  } finally {
+    stopLoading()
+  }
 }
 
-function updateEntry(entry: TodoEntry) {
-  // Think that async/await would be less clear
-  openModal<EditTodoEntryData, typeof TodoEditModal>(TodoEditModal, {
-    name: entry.name,
-    status: entry.status,
-    title: "Update TODO",
-  })
-    .then(updateData => {
-      const updatedEntry: TodoEntry = { id: entry.id, ...updateData }
-      loader = loading.show()
-      return TodoService.patchTodo(updatedEntry)
-    })
-    .then(updated => {
-      // The ids should match
-      if (updated.id != entry.id) throw Error()
-      entries.value.set(updated.id, updated)
-      toast("Update successful!", { type: TYPE.INFO })
-    })
-    .catch(error => {
-      console.log(error)
-      showNetworkErrorToast()
-    })
-    .finally(() => {
-      loader.hide()
-    })
+async function updateEntry(entry: TodoEntry) {
+  let modalData
+  try {
+    modalData = await openModal<EditTodoEntryData, typeof TodoEditModal>(
+      TodoEditModal,
+      {
+        name: entry.name,
+        status: entry.status,
+        title: "Update TODO",
+      },
+    )
+  } catch {
+    return
+  }
+  startLoading()
+  try {
+    const updatedEntry: TodoEntry = { id: entry.id, ...modalData }
+    const patchedEntry = await TodoService.patchTodo(updatedEntry)
+    if (patchedEntry.id != entry.id) throw Error()
+    entries.value.set(patchedEntry.id, patchedEntry)
+    toast("Update successful!", { type: TYPE.INFO })
+  } catch {
+    showNetworkErrorToast()
+  } finally {
+    stopLoading()
+  }
 }
 
 async function changeStatus(entry: TodoEntry) {
   loader = loading.show()
-  const updatedEntry: TodoEntry = {
-    id: entry.id,
-    name: entry.name,
-    status: TodoService.nextTodoState(entry.status),
-  }
+  const updatedEntry: TodoEntry = { ...entry }
+  updatedEntry.status = TodoService.nextTodoState(entry.status)
   try {
     await TodoService.patchTodo(updatedEntry)
     entry.status = updatedEntry.status
